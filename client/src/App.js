70979aab6d8fb6e1507905723574d0d37a75a238
amar PC <amarvadla27@gmail.com>
2020-02-24 23:28:17 +0530

    completed routes, boiler plate of react.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/navbar';
+import Landing from './components/layout/landing';
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
